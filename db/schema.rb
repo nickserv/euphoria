26c6906fb53e461797242e9650a92ac2edf69ed7
Nicolas McCurdy <thenickperson@gmail.com>
2013-10-27 03:14:20 -0400

    Update the db schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131027064200) do
+ActiveRecord::Schema.define(version: 20131027070629) do
+
+  create_table "items", force: true do |t|
+    t.string   "name"
+    t.string   "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "rooms", force: true do |t|
+    t.string   "name"
+    t.string   "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "worlds", force: true do |t|
     t.string   "name"
