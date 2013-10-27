81a3191145bea39ebbc134216977813a6de57978
Nicolas McCurdy <thenickperson@gmail.com>
2013-10-27 02:21:35 -0400

    Generate a new Rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Euphoria::Application.config.secret_key_base = 'df3315fd2553c9e8c36cc59ab9d0cb693a7709497368bcece69d4841f5525927e2c8739d570044626257bfb6f62ef9116a8d8937242df71fe690997d5e08651f'
