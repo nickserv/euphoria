da673dae59725bf94ea2b6588801ced3bea5d00c
Nicolas McCurdy <thenickperson@gmail.com>
2013-10-27 04:09:19 -0400

    Add some more manual setup for Devise

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,4 +26,9 @@ Euphoria::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+
+  # This should be defined for Devise.
+  # In production, :host should be set to the actual host of your application.
+  config.action_mailer.default_url_options = { host: 'localhost:3000' }
+
 end
