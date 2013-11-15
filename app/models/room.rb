6eac9687f996c31af40422e2ffae3453398362d4
Nicolas McCurdy <thenickperson@gmail.com>
2013-11-15 03:59:13 -0500

    Set up the relationships between Rooms through Connections

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,5 +1,7 @@
 class Room < ActiveRecord::Base
   belongs_to :world
+  has_many :connections, foreign_key: :source_id
+  has_many :neighbors, through: :connections, source: :source
 
   validates :name,
             presence: true,
