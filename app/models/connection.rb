0ffc55f48d749ad2711eec44e7e722fcde6c2a46
Nicolas McCurdy <thenickperson@gmail.com>
2013-11-16 21:07:44 -0500

    Add validations for connection directions

--- app/models/connection.rb
+++ app/models/connection.rb
@@ -1,4 +1,11 @@
 class Connection < ActiveRecord::Base
   belongs_to :source, class_name: :Room
   belongs_to :destination, class_name: :Room
+
+  validates :direction,
+            presence: true,
+            inclusion: {
+              in: %w(north south east west),
+              message: '%{value} is not a valid direction'
+            }
 end
